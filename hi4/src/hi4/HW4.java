e8903614f5cb0312b3c5faf505d097fecae36d2c
louol1 <160557324+louol1@users.noreply.github.com>
2024-03-19 12:08:06 +0800

    Add files via upload

---
+++ hi4/src/hi4/HW4.java
@@ -0,0 +1,30 @@
+package hi4;
+import java.util.Scanner;
+
+public class HW4 {
+
+	public static void main(String[] args) {
+		String temp = "";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("這是可以陪你聊程式!你可以問我一些問題");
+		   while(true) {
+			   System.out.print("你說:");
+			   temp=sc.nextLine();
+			   temp=temp.replace("嗎", "");
+			   temp=temp.replace("?", "!");
+			   temp=temp.replace("會不會", "會");
+			   temp=temp.replace("能不能", "能");
+			   temp=temp.replace("你能", "我能");
+			   temp=temp.replace("你會", "我會");
+			   System.out.println("我說:"+temp);
+			   
+			   
+			   if(temp.equals("0")) {
+				   System.out.println("離開聊天程序");
+				   break;
+			   }
+		   }
+
+	}
+
+}
